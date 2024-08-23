7636c267ff4b0526161e3caba5f1e45e40c1a4c1
nguyenthong11 <thong1988@icloud.com>
2024-08-23 23:52:27 +0200

    MiniProject

--- main.py
+++ main.py
@@ -13,36 +13,36 @@ def main() -> None:
         print('4. Morse code convert')
         print('5. Currency rate convert')
         print('q. Exit')
-        choice = input("Enter your choice: ")
-        if choice == '1':
-            finance()
-        elif choice == '2':
-            check_url()
-        elif choice == '3':
-            word_count()
-        elif choice == '4':
-            while True:
-                print('1. Text to Morse')
-                print('2. Morse to text')
-                print('0. Return')
-                select = input("You select: ")
-                if select == '1':
-                    t = input('Text: ')
-                    print(Morse_code.text2morse(t))
-                elif select == '2':
-                    t = input('Morse code: ')
-                    print(Morse_code.morse2text(t))
-                elif select == '0':
-                    break
-                else:
-                    print("Invalid choice")
-        elif choice == '5':
-            exec(open("CurrencyConvert.py").read())
-        elif choice == 'q':
-            print('Exiting...')
-            break
-        else:
-            print("Invalid choice.")
+        match input("Enter your choice: "):
+            case '1':
+                finance()
+            case '2':
+                check_url()
+            case '3':
+                word_count()
+            case '4':
+                while True:
+                    print('1. Text to Morse')
+                    print('2. Morse to text')
+                    print('0. Return')
+                    select = input("You select: ")
+                    if select == '1':
+                        t = input('Text: ')
+                        print(Morse_code.text2morse(t))
+                    elif select == '2':
+                        t = input('Morse code: ')
+                        print(Morse_code.morse2text(t))
+                    elif select == '0':
+                        break
+                    else:
+                        print("Invalid choice")
+            case '5':
+                exec(open("CurrencyConvert.py").read())
+            case 'q':
+                print('Exiting...')
+                break
+            case _:
+                print("Invalid choice.")
 
 
 if __name__ == '__main__':
